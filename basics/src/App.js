e2b844f70c538c0663d7db41baf3a0492fe7aa48
Sunil5883 <gsunil2088@gmail.com>
2024-12-26 12:50:45 +0530

    changed

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,17 @@
-
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
+import Greeting from "./Greetings";
 function App() {
   return (
+    <div>
   <h1>Hello Guys</h1>
+  <Greeting name = {'Akash'} />
+  <Greeting name = {'Ayush'} />
+  <Home name = {'Sunil'} />
+  <Contact />
+  <About />
+  </div>
 );
 }
 
---
+++ basics/src/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home(props) {
+    return (
+        <div>
+            <h1>Welcome Home {props.name} </h1>
+        </div>
+    )
+}
